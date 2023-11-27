c4d34d2334ed503a2947a340d1e7c79b810c54ab
Zac Lippard <zacl@zillowgroup.com>
2023-11-27 11:56:32 -0800

    Chapter 3: FPE - Remove TOML

--- 03-android-fundamentals/projects/final/build.gradle.kts
+++ 03-android-fundamentals/projects/final/build.gradle.kts
@@ -34,6 +34,6 @@
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-  alias(libs.plugins.android.application) apply false
-  alias(libs.plugins.kotlin.android) apply false
+  id("com.android.application") version "8.1.4" apply false
+  id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
