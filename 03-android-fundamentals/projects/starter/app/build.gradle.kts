7bd903b20614617f16b05bca0a34fb25bf509bc4
Zac Lippard <zacl@zillowgroup.com>
2023-12-26 18:57:58 -0800

    FPE: Chapter 3 - align Java, Kotlin and Compose compiler versions.

--- 03-android-fundamentals/projects/starter/app/build.gradle.kts
+++ 03-android-fundamentals/projects/starter/app/build.gradle.kts
@@ -64,11 +64,11 @@ android {
     }
   }
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
   }
   kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "17"
   }
   buildFeatures {
     compose = true
@@ -86,8 +86,8 @@ android {
 dependencies {
   implementation("androidx.core:core-ktx:1.12.0")
   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-  implementation("androidx.activity:activity-compose:1.7.2")
-  implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+  implementation("androidx.activity:activity-compose:1.8.2")
+  implementation(platform("androidx.compose:compose-bom:2023.10.00"))
   implementation("androidx.compose.ui:ui")
   implementation("androidx.compose.ui:ui-graphics")
   implementation("androidx.compose.ui:ui-tooling-preview")
@@ -95,7 +95,7 @@ dependencies {
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-  androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+  androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.00"))
   androidTestImplementation("androidx.compose.ui:ui-test-junit4")
   debugImplementation("androidx.compose.ui:ui-tooling")
   debugImplementation("androidx.compose.ui:ui-test-manifest")
--- 03-android-fundamentals/projects/starter/settings.gradle.kts
+++ 03-android-fundamentals/projects/starter/settings.gradle.kts
@@ -33,18 +33,18 @@
  */
 
 pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
+  repositories {
+      google()
+      mavenCentral()
+      gradlePluginPortal()
+  }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    google()
+    mavenCentral()
+  }
 }
 
 rootProject.name = "Kodeco Chat"
