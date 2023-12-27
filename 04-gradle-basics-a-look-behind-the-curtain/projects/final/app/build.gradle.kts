3d3481fee2d6d2f1bf5eff538738c5de87a53e72
Zac Lippard <zacl@zillowgroup.com>
2023-12-26 19:46:52 -0800

    FPE: Chapter 4 - Align Java, Kotlin and Compose compiler versions

--- 04-gradle-basics-a-look-behind-the-curtain/projects/final/app/build.gradle.kts
+++ 04-gradle-basics-a-look-behind-the-curtain/projects/final/app/build.gradle.kts
@@ -45,8 +45,12 @@ android {
       applicationIdSuffix = ".debug"
     }
   }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
   kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "17"
   }
   buildFeatures {
     compose = true
