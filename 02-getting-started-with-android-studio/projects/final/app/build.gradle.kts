b5a25f3edde6fffe85004ce02c141d1c12f8929b
Zac Lippard <zacl@zillowgroup.com>
2023-12-26 18:17:16 -0800

    FPE: Chapter 2 aligning Java, Kotlin and Compose Compiler versions

--- 02-getting-started-with-android-studio/projects/final/app/build.gradle.kts
+++ 02-getting-started-with-android-studio/projects/final/app/build.gradle.kts
@@ -30,17 +30,17 @@ android {
     }
   }
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
   }
   kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "17"
   }
   buildFeatures {
     compose = true
   }
   composeOptions {
-    kotlinCompilerExtensionVersion = "1.5.1"
+    kotlinCompilerExtensionVersion = "1.5.3"
   }
   packaging {
     resources {
@@ -53,8 +53,8 @@ dependencies {
 
   implementation("androidx.core:core-ktx:1.12.0")
   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-  implementation("androidx.activity:activity-compose:1.7.2")
-  implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+  implementation("androidx.activity:activity-compose:1.8.2")
+  implementation(platform("androidx.compose:compose-bom:2023.10.00"))
   implementation("androidx.compose.ui:ui")
   implementation("androidx.compose.ui:ui-graphics")
   implementation("androidx.compose.ui:ui-tooling-preview")
@@ -62,7 +62,7 @@ dependencies {
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-  androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+  androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.00"))
   androidTestImplementation("androidx.compose.ui:ui-test-junit4")
   debugImplementation("androidx.compose.ui:ui-tooling")
   debugImplementation("androidx.compose.ui:ui-test-manifest")
